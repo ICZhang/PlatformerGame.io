30ccca8d58add7b706de41662cb97dd5788eb501
ICZhang <TICZhang8239@gmail.com>
2025-09-21 23:18:02 -0400

    Update something

--- game.js
+++ game.js
@@ -203,9 +203,9 @@ function draw() {
     //text(arrowListDown[0].x, 200,200);
     background(138, 176, 226);
     fill("green");
-    dynamicCloudSprite.x+=5;
-    dynamicCloudSprite2.x+=5;
-    if(dynamicCloudSprite.x > 5000) dynamicCloudSprite.x = -400;
+    //dynamicCloudSprite.x+=5;
+    //dynamicCloudSprite2.x+=5;
+    if(dynamicCloudSprite.x > 2100) dynamicCloudSprite.x = -400;
     //if(dynamicCloudSprite2.x > 1500) dynamicCloudSprite2 = 0;
     text("C: " + dynamicCloudSprite.x, 100, 100);
     text("C2: " + dynamicCloudSprite2.x, 100, 200);
